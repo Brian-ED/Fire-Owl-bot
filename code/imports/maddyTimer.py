ccaa21621e97c98df164c324a14bea53ba5f2adb
Brian-ED <brianellingsgaard9@gmail.com>
2022-12-22 20:42:20 +0000

    finished updating maddyTimer

--- code/imports/maddyTimer.py
+++ code/imports/maddyTimer.py
@@ -3,20 +3,19 @@ from asyncio import sleep as asySleep
 from datetime import datetime
 import pytz
 
-maddyTimezone=pytz.timezone("EET")
-
-async def WaitTill(send,*time):
-    dt = datetime.now(maddyTimezone)
-    while (dt.hour,dt.minute)!=time:
-        print("Time info: ",dt.hour,dt.minute,time)
-        dt = datetime.now(maddyTimezone)
-        await asySleep(40)
-    print("Notif sent")
-    await send("Clean your teeth and have fun tying yourself up")
-
 async def maddyTimerMain(client):
+    maddyTimezone=pytz.timezone("EET")
     print("Auto sender started")
     maddy=await client.fetch_user(633637569975943169)
     while 1:
-        await WaitTill(maddy.send,7,45)
-        await WaitTill(maddy.send,*map(randrange,(18,0),(22,59)))
+        
+        times=(7,45),(*map(randrange,(18,0),(22,59)),)
+        dt = datetime.now(maddyTimezone)
+        
+        while (dt.hour,dt.minute)not in times:
+            dt = datetime.now(maddyTimezone)
+            await asySleep(40)
+        
+        print("Notif sent")
+        await maddy.send("Clean your teeth and have fun tying yourself up")
+        await asySleep(200)
